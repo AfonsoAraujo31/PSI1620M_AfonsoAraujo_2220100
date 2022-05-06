f65920c6f5ee0834e638c927d5061d0fb779de2e
Afonso Soares Araujo <afonso16araujo@gmail.com>
2022-05-06 22:48:43 +0100

    Erro ao ligar á base de dados

--- src/DLS-ALFEITE/DLS-ALFEITE/Form1.Designer.cs
+++ src/DLS-ALFEITE/DLS-ALFEITE/Form1.Designer.cs
@@ -30,7 +30,6 @@ namespace DLS_ALFEITE
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Frm_login));
-            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
@@ -38,34 +37,26 @@ namespace DLS_ALFEITE
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.panel1 = new System.Windows.Forms.Panel();
             this.panel2 = new System.Windows.Forms.Panel();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.pictureBox4 = new System.Windows.Forms.PictureBox();
-            this.pictureBox5 = new System.Windows.Forms.PictureBox();
-            this.pictureBox6 = new System.Windows.Forms.PictureBox();
             this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.textBox_username = new System.Windows.Forms.TextBox();
+            this.textBox_password = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.btn_login = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btn_login)).BeginInit();
             this.SuspendLayout();
-            // 
-            // flowLayoutPanel2
-            // 
-            this.flowLayoutPanel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
-            this.flowLayoutPanel2.Location = new System.Drawing.Point(0, -2);
-            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
-            this.flowLayoutPanel2.Size = new System.Drawing.Size(828, 50);
-            this.flowLayoutPanel2.TabIndex = 0;
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(45, 57);
+            this.pictureBox1.Location = new System.Drawing.Point(30, 37);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(277, 347);
+            this.pictureBox1.Size = new System.Drawing.Size(185, 226);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
@@ -74,9 +65,10 @@ namespace DLS_ALFEITE
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Arial Black", 18F, System.Drawing.FontStyle.Bold);
-            this.label1.Location = new System.Drawing.Point(435, 93);
+            this.label1.Location = new System.Drawing.Point(290, 60);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(280, 51);
+            this.label1.Size = new System.Drawing.Size(186, 33);
             this.label1.TabIndex = 2;
             this.label1.Text = "DLS-ALFEITE";
             // 
@@ -84,18 +76,20 @@ namespace DLS_ALFEITE
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Arial Black", 8.25F, System.Drawing.FontStyle.Bold);
-            this.label2.Location = new System.Drawing.Point(383, 144);
+            this.label2.Location = new System.Drawing.Point(255, 94);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(384, 24);
+            this.label2.Size = new System.Drawing.Size(253, 15);
             this.label2.TabIndex = 3;
             this.label2.Text = "DEPARTAMENTO LOGÍSTICO SANITÁRIO";
             // 
             // pictureBox2
             // 
             this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(441, 210);
+            this.pictureBox2.Location = new System.Drawing.Point(294, 136);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(50, 35);
+            this.pictureBox2.Size = new System.Drawing.Size(33, 23);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox2.TabIndex = 4;
             this.pictureBox2.TabStop = false;
@@ -103,9 +97,10 @@ namespace DLS_ALFEITE
             // pictureBox3
             // 
             this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
-            this.pictureBox3.Location = new System.Drawing.Point(441, 264);
+            this.pictureBox3.Location = new System.Drawing.Point(294, 169);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(50, 35);
+            this.pictureBox3.Size = new System.Drawing.Size(33, 23);
             this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox3.TabIndex = 5;
             this.pictureBox3.TabStop = false;
@@ -113,92 +108,116 @@ namespace DLS_ALFEITE
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.Black;
-            this.panel1.Location = new System.Drawing.Point(441, 245);
+            this.panel1.Location = new System.Drawing.Point(294, 159);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(267, 3);
+            this.panel1.Size = new System.Drawing.Size(178, 2);
             this.panel1.TabIndex = 6;
             // 
             // panel2
             // 
             this.panel2.BackColor = System.Drawing.Color.Black;
-            this.panel2.Location = new System.Drawing.Point(441, 296);
+            this.panel2.Location = new System.Drawing.Point(294, 192);
+            this.panel2.Margin = new System.Windows.Forms.Padding(2);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(267, 3);
+            this.panel2.Size = new System.Drawing.Size(178, 2);
             this.panel2.TabIndex = 7;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Arial", 9F);
-            this.label3.Location = new System.Drawing.Point(497, 224);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(92, 21);
-            this.label3.TabIndex = 8;
-            this.label3.Text = "Username";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Arial", 9F);
-            this.label4.Location = new System.Drawing.Point(497, 273);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(89, 21);
-            this.label4.TabIndex = 9;
-            this.label4.Text = "Password";
-            // 
-            // pictureBox4
-            // 
-            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
-            this.pictureBox4.Location = new System.Drawing.Point(659, 263);
-            this.pictureBox4.Name = "pictureBox4";
-            this.pictureBox4.Size = new System.Drawing.Size(49, 33);
-            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox4.TabIndex = 10;
-            this.pictureBox4.TabStop = false;
-            // 
-            // pictureBox5
-            // 
-            this.pictureBox5.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox5.Image")));
-            this.pictureBox5.Location = new System.Drawing.Point(659, 263);
-            this.pictureBox5.Name = "pictureBox5";
-            this.pictureBox5.Size = new System.Drawing.Size(49, 33);
-            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox5.TabIndex = 11;
-            this.pictureBox5.TabStop = false;
-            // 
-            // pictureBox6
-            // 
-            this.pictureBox6.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox6.Image")));
-            this.pictureBox6.Location = new System.Drawing.Point(516, 296);
-            this.pictureBox6.Name = "pictureBox6";
-            this.pictureBox6.Size = new System.Drawing.Size(137, 76);
-            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
-            this.pictureBox6.TabIndex = 12;
-            this.pictureBox6.TabStop = false;
             // 
             // linkLabel1
             // 
             this.linkLabel1.AutoSize = true;
-            this.linkLabel1.Location = new System.Drawing.Point(704, 394);
+            this.linkLabel1.Location = new System.Drawing.Point(474, 255);
+            this.linkLabel1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.linkLabel1.Name = "linkLabel1";
-            this.linkLabel1.Size = new System.Drawing.Size(125, 20);
+            this.linkLabel1.Size = new System.Drawing.Size(84, 13);
             this.linkLabel1.TabIndex = 13;
             this.linkLabel1.TabStop = true;
             this.linkLabel1.Text = "Reset Password";
             // 
+            // textBox_username
+            // 
+            this.textBox_username.BackColor = System.Drawing.Color.Azure;
+            this.textBox_username.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox_username.Location = new System.Drawing.Point(327, 145);
+            this.textBox_username.Name = "textBox_username";
+            this.textBox_username.Size = new System.Drawing.Size(100, 13);
+            this.textBox_username.TabIndex = 14;
+            this.textBox_username.Text = "Username";
+            this.textBox_username.Enter += new System.EventHandler(this.textBox_username_Enter_1);
+            this.textBox_username.Leave += new System.EventHandler(this.textBox_username_Leave);
+            // 
+            // textBox_password
+            // 
+            this.textBox_password.BackColor = System.Drawing.Color.Azure;
+            this.textBox_password.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox_password.Location = new System.Drawing.Point(327, 178);
+            this.textBox_password.Name = "textBox_password";
+            this.textBox_password.Size = new System.Drawing.Size(100, 13);
+            this.textBox_password.TabIndex = 15;
+            this.textBox_password.Text = "Password";
+            this.textBox_password.Enter += new System.EventHandler(this.textBox_password_Enter_1);
+            this.textBox_password.Leave += new System.EventHandler(this.textBox_password_Leave);
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.Color.Azure;
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.Location = new System.Drawing.Point(439, 165);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(33, 26);
+            this.button1.TabIndex = 16;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.ForeColor = System.Drawing.Color.Azure;
+            this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
+            this.button2.Location = new System.Drawing.Point(439, 166);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(33, 26);
+            this.button2.TabIndex = 17;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.panel3.Location = new System.Drawing.Point(0, -1);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(556, 30);
+            this.panel3.TabIndex = 18;
+            // 
+            // btn_login
+            // 
+            this.btn_login.Image = ((System.Drawing.Image)(resources.GetObject("btn_login.Image")));
+            this.btn_login.Location = new System.Drawing.Point(333, 192);
+            this.btn_login.Margin = new System.Windows.Forms.Padding(2);
+            this.btn_login.Name = "btn_login";
+            this.btn_login.Size = new System.Drawing.Size(96, 48);
+            this.btn_login.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.btn_login.TabIndex = 12;
+            this.btn_login.TabStop = false;
+            this.btn_login.Click += new System.EventHandler(this.btn_login_Click);
+            // 
             // Frm_login
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
             this.AutoValidate = System.Windows.Forms.AutoValidate.EnablePreventFocusChange;
             this.BackColor = System.Drawing.Color.Azure;
-            this.ClientSize = new System.Drawing.Size(828, 416);
+            this.ClientSize = new System.Drawing.Size(556, 270);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox_password);
             this.Controls.Add(this.linkLabel1);
-            this.Controls.Add(this.pictureBox4);
-            this.Controls.Add(this.pictureBox5);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.pictureBox3);
@@ -206,27 +225,25 @@ namespace DLS_ALFEITE
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.flowLayoutPanel2);
-            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.btn_login);
+            this.Controls.Add(this.textBox_username);
+            this.Controls.Add(this.button1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.HelpButton = true;
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Frm_login";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "on";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btn_login)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
@@ -234,12 +251,13 @@ namespace DLS_ALFEITE
         private System.Windows.Forms.PictureBox pictureBox3;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.PictureBox pictureBox4;
-        private System.Windows.Forms.PictureBox pictureBox5;
-        private System.Windows.Forms.PictureBox pictureBox6;
         private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.TextBox textBox_username;
+        private System.Windows.Forms.TextBox textBox_password;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.PictureBox btn_login;
     }
 }
 
--- src/DLS-ALFEITE/DLS-ALFEITE/Form1.cs
+++ src/DLS-ALFEITE/DLS-ALFEITE/Form1.cs
@@ -1,22 +1,102 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.Data.SqlClient;
 
 namespace DLS_ALFEITE
 {
     public partial class Frm_login : Form
     {
+        private string ContionString;
         public Frm_login()
         {
             InitializeComponent();
         }
+        private void btn_login_Click(object sender, EventArgs e)
+        {
+            if (textBox_username.Text == "" || textBox_password.Text == "")
+            {
+                MessageBox.Show("Insira o username ou password");
+                return;
+            }
+            try
+            {
+                SqlConnection sqlConn = new SqlConnection(ContionString);
+                SqlCommand cmd = sqlConn.CreateCommand();
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = $"SELECT username,password FROM login_utilizadores WHERE username='" + textBox_username.Text + "' AND password='" + textBox_password.Text + "'";
+                sqlConn.Open();
+                var dr = cmd.ExecuteReader();
+                if (dr.HasRows)
+                {
+                    MessageBox.Show("asas");
+                }
+                sqlConn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro:{0}",ex.Message);
+            }
+        }
+
+        private void textBox_password_Enter_1(object sender, EventArgs e)
+        {
+            if (textBox_password.Text == "Password")
+            {
+                textBox_password.Text = "";
+                textBox_password.PasswordChar = '●';
+            }
+        }
+        private void textBox_password_Leave(object sender, EventArgs e)
+        {
+            if (textBox_password.Text == "")
+            {
+                textBox_password.Text = "Password";
+                textBox_password.PasswordChar = '\0';
+            }
+        }
+        
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            if (textBox_password.PasswordChar == '●')
+            {
+                button1.BringToFront();
+                textBox_password.PasswordChar = '\0';
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox_password.PasswordChar == '\0')
+            {
+                button2.BringToFront();
+                textBox_password.PasswordChar = '●';
+            }
+        }
 
         
+
+        private void textBox_username_Enter_1(object sender, EventArgs e)
+        {
+            if (textBox_username.Text == "Username")
+            {
+                textBox_username.Text = "";
+            }
+        }
+
+        private void textBox_username_Leave(object sender, EventArgs e)
+        {
+
+        }
     }
 }
+
+
